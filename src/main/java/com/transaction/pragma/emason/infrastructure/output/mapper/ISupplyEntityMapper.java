bf95a12d1a157c8fff4524d5d661429e10d339b1
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2024-09-26 23:53:00 -0500

    refactor: remove unused import

--- src/main/java/com/transaction/pragma/emason/infrastructure/output/mapper/ISupplyEntityMapper.java
+++ src/main/java/com/transaction/pragma/emason/infrastructure/output/mapper/ISupplyEntityMapper.java
@@ -4,7 +4,6 @@ package com.transaction.pragma.emason.infrastructure.output.mapper;
 import com.transaction.pragma.emason.domain.model.Supply;
 import com.transaction.pragma.emason.infrastructure.output.entity.SupplyEntity;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring",
