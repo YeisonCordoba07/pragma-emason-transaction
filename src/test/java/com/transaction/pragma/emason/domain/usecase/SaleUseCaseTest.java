626dba2da5371c12496fb6a4da723b1acf8c73e2
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2024-10-02 15:13:00 -0500

    refactor: remove unused imports

--- src/test/java/com/transaction/pragma/emason/domain/usecase/SaleUseCaseTest.java
+++ src/test/java/com/transaction/pragma/emason/domain/usecase/SaleUseCaseTest.java
@@ -8,7 +8,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import static org.junit.jupiter.api.Assertions.*;
+
 import static org.mockito.Mockito.verify;
 
 class SaleUseCaseTest {
